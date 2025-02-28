1424cb7240a0df4c5067efdf97ea8828db597b01
JeanVittory <vittory.dev@gmail.com>
2025-02-28 11:34:19 -0500

    feat: :sparkles: Add a file to retrieve the environment variables and share them across the project

---
+++ app/config/dotenv.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+PROJECT_ID = os.getenv('PROJECT_ID')
+SESSION_ID = os.getenv('SESSION_ID')
+COLLECTION_NAME = os.getenv('COLLECTION_NAME')
+TEMPERATURE_MODEL = os.getenv('TEMPERATURE_MODEL')
+MODEL_NAME = os.getenv('MODEL_NAME')
+MAX_TOKENS = os.getenv('MAX_TOKENS')
