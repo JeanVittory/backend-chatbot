24134d55aa39863b09bc8b8794bb325975bf3ab7
JeanVittory <vittory.dev@gmail.com>
2025-02-28 11:35:31 -0500

    feat: :art: Add MAX_TOKENS from the environment variables and improve the naming of the property into the request

--- app/middlewares/validate_tokens.py
+++ app/middlewares/validate_tokens.py
@@ -1,11 +1,12 @@
 from fastapi import Request, HTTPException
 from app.models.message import Message
+from app.config.dotenv import MAX_TOKENS
 
 async def validate_tokens(request:Request):
     try:
         json = await request.json()
         body = Message(**json)
-        if len(body.message) > 100:
+        if len(body.content) > int(MAX_TOKENS):
             raise HTTPException(status_code=400, detail="Too many tokens found.")
         return body
     except ValueError as e:
