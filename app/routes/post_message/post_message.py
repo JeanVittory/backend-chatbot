0a8a3de4c70f7ca077817b28820491679182a701
JeanVittory <vittory.dev@gmail.com>
2025-02-28 11:40:42 -0500

    feat: :art: Implement the send_message function and add a try...except to handle errors

--- app/routes/post_message/post_message.py
+++ app/routes/post_message/post_message.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.middlewares.validate_tokens import validate_tokens
 from app.models.message import Message
+from app.services.send_message import send_message
 
 router = APIRouter(prefix="/message", tags=["message"])
 
-@router.post("/" )
+@router.post("/", status_code=200)
 async def post_message(message: Message = Depends(validate_tokens)):
-    print("Hello from handler", message.message)
-    return {"message": "Message received"}
+    try:
+        ai_response = send_message(message.content)
+        return {"message": ai_response}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
