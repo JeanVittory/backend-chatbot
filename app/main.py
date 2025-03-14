f42e53ba1290b61191fd85ed6e26d0ba5660e11b
JeanVittory <vittory.dev@gmail.com>
2025-03-14 17:46:17 -0500

    feat: :sparkles: Add new route get_message

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .routes.post_message import post_message
+from .routes.get_messages import get_message
 
 app = FastAPI()
 app.include_router(post_message.router)
+app.include_router(get_message.router)
