07ae4c4428cb40a47be2dc3a6a6486a1f6970faf
JeanVittory <vittory.dev@gmail.com>
2025-03-14 17:45:49 -0500

    feat: :sparkles: Add an initial AI message and context if there is not previous messages in the chat

--- app/services/send_message.py
+++ app/services/send_message.py
@@ -1,9 +1,10 @@
 from google.cloud import firestore
 from langchain_google_firestore import FirestoreChatMessageHistory
 from langchain_groq import ChatGroq
-from langchain_core.messages import AIMessage
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from app.utils.numeric_parser import parser
 from app.config.dotenv import GROQ_API_KEY, PROJECT_ID, SESSION_ID, COLLECTION_NAME,TEMPERATURE_MODEL, MODEL_NAME, MAX_TOKENS 
+import datetime
 
 def send_message(message:str):
     client = firestore.Client(project=PROJECT_ID)
@@ -12,9 +13,15 @@ def send_message(message:str):
     max_tokens_parsed = parser(MAX_TOKENS)
     model = ChatGroq(api_key=GROQ_API_KEY, temperature=temperature_parsed, model=MODEL_NAME, max_tokens=max_tokens_parsed)
     
-    chat.add_user_message(message)
+    if not chat.messages: 
+        system_message = SystemMessage(content="You're a useful and friendly assitant. Answer clearly and precisely.")
+        ai_message = AIMessage(content="Hello, how can I help you today?", additional_kwargs={"date_time": datetime.datetime.now()})
+        chat.add_messages([system_message, ai_message])
+    
+    user_message = HumanMessage(content=message, additional_kwargs={"date_time": datetime.datetime.now()})
+    chat.add_user_message(user_message)
     ai_response = model.invoke(chat.messages)
-    ai_message = AIMessage(ai_response.content)
+    ai_message = AIMessage(content=ai_response.content, additional_kwargs={"date_time": datetime.datetime.now()})
     chat.add_ai_message(ai_message)
     
     return ai_response.content
