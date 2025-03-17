f81f0fd23a225aec3c2df406d834249ac3e68f0f
JeanVittory <vittory.dev@gmail.com>
2025-03-16 20:53:28 -0500

    feat: :sparkles: Include websocket as a router endpoint in FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from .routes.post_message import post_message
 from .routes.get_messages import get_message
+from .routes.websocket import router
 
 app = FastAPI()
 app.include_router(post_message.router)
 app.include_router(get_message.router)
+app.include_router(router)
