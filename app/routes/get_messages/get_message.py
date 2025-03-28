4d7237f14eb320242771bf859b1d71fbfe2649cb
JeanVittory <vittory.dev@gmail.com>
2025-03-28 15:51:27 -0500

    feat: :sparkles: Add userId as path parameter following a semantic REST structure

--- app/routes/get_messages/get_message.py
+++ app/routes/get_messages/get_message.py
@@ -3,10 +3,10 @@ from app.services.get_messages import get_messages
 
 router = APIRouter(prefix="/messages", tags=["messages"])
 
-@router.get("/", status_code=200)
-async def get_message():
+@router.get("/{userId}", status_code=200)
+async def get_message(userId:str):
     try:
-        history_messages = get_messages()
+        history_messages = get_messages(userId)
         return {"message": history_messages}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
