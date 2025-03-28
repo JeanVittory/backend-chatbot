7f6eacf837585cee5be0be7025a6b6c0388b6305
JeanVittory <vittory.dev@gmail.com>
2025-03-28 17:37:19 -0500

    feat: :sparkles: Add uuid as additional_kwargs in every message to indetify them easily

--- app/services/send_message.py
+++ app/services/send_message.py
@@ -5,6 +5,7 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from app.utils.numeric_parser import parser
 from app.config.dotenv import GROQ_API_KEY, PROJECT_ID, SESSION_ID, COLLECTION_NAME,TEMPERATURE_MODEL, MODEL_NAME, MAX_TOKENS 
 import datetime
+import uuid
 
 def send_message(message:str):
     client = firestore.Client(project=PROJECT_ID)
@@ -15,13 +16,13 @@ def send_message(message:str):
     
     if not chat.messages: 
         system_message = SystemMessage(content="You're a useful and friendly assitant. Answer clearly and precisely.")
-        ai_message = AIMessage(content="Hello, how can I help you today?", additional_kwargs={"date_time": datetime.datetime.now()})
+        ai_message = AIMessage(content="Hello, how can I help you today?", additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
         chat.add_messages([system_message, ai_message])
     
-    user_message = HumanMessage(content=message, additional_kwargs={"date_time": datetime.datetime.now()})
+    user_message = HumanMessage(content=message, additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
     chat.add_user_message(user_message)
     ai_response = model.invoke(chat.messages)
-    ai_message = AIMessage(content=ai_response.content, additional_kwargs={"date_time": datetime.datetime.now()})
+    ai_message = AIMessage(content=ai_response.content, additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
     chat.add_ai_message(ai_message)
     
     return ai_response.content
