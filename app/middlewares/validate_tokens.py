8666543a1c59063cc9c970124313625f7290e943
JeanVittory <vittory.dev@gmail.com>
2025-03-28 15:50:49 -0500

    feat: :sparkles: Add message as string following the socket configuration

--- app/middlewares/validate_tokens.py
+++ app/middlewares/validate_tokens.py
@@ -2,12 +2,10 @@ from fastapi import Request, HTTPException
 from app.models.message import Message
 from app.config.dotenv import MAX_TOKENS
 
-async def validate_tokens(request:Request):
+async def validate_tokens(message:Request):
     try:
-        json = await request.json()
-        body = Message(**json)
-        if len(body.content) > int(MAX_TOKENS):
+        if len(message) > int(MAX_TOKENS):
             raise HTTPException(status_code=400, detail="Too many tokens found.")
-        return body
+        return message
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
