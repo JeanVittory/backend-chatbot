22e6aeda4bc2abb9826f074f8d2c1e5d91882071
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:25:48 -0500

    feat: :fire: Remove SESSION_ID from env variables

--- app/config/dotenv.py
+++ app/config/dotenv.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 PROJECT_ID = os.getenv('PROJECT_ID')
-SESSION_ID = os.getenv('SESSION_ID')
 COLLECTION_NAME = os.getenv('COLLECTION_NAME')
 TEMPERATURE_MODEL = os.getenv('TEMPERATURE_MODEL')
 MODEL_NAME = os.getenv('MODEL_NAME')
