40fd849edf3de18e37816d8710abacdc36a0abb0
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:16:56 -0500

    feat: :sparkles: Add a user validation in the socket connection

--- app/routes/websocket/websocket.py
+++ app/routes/websocket/websocket.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.middlewares.validate_tokens import validate_tokens
+from app.services.validate_user import validate_user
 from app.services.send_message import send_message
 
 router = APIRouter()
@@ -8,13 +9,18 @@ router = APIRouter()
 async def websocket(websocket: WebSocket):
     await websocket.accept() 
     try:
+        user_id = await websocket.receive_text()
+        is_a_user_valid = validate_user(user_id)
+        if not is_a_user_valid:
+            raise WebSocketDisconnect() 
+        
         while True:
             message = await websocket.receive_text()
             await validate_tokens(message)
-            ai_response = send_message(message)
+            ai_response = send_message(message, user_id)
             await websocket.send_text(ai_response)
-    except WebSocketDisconnect:
-        await websocket.send_text("WebSocket disconnected")
+    except WebSocketDisconnect as e:
+        await websocket.send_text("WebSocket disconnected", e)
     except Exception as e:
         await websocket.send_text(f"An error occurred: {str(e)}")
     finally:
