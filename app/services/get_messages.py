eedcd909f6daf45f0dabd571081d2f84c0d58164
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:18:29 -0500

    feat: :sparkles: Add a user_id value in the FirestoreChatMessageHistory and validate if the user do not have any previous messages. In other hand we filter the system context message  from the final response

--- app/services/get_messages.py
+++ app/services/get_messages.py
@@ -1,13 +1,21 @@
 from google.cloud import firestore
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_google_firestore import FirestoreChatMessageHistory
-from app.config.dotenv import PROJECT_ID, SESSION_ID, COLLECTION_NAME
+from app.config.dotenv import PROJECT_ID, COLLECTION_NAME
+import datetime
+import uuid
 
-def get_messages(userId:str):
+def get_messages(user_id:str):
     client = firestore.Client(project=PROJECT_ID)
-    chat = FirestoreChatMessageHistory(client=client, session_id=userId, collection=COLLECTION_NAME)
+    chat = FirestoreChatMessageHistory(client=client, session_id=user_id, collection=COLLECTION_NAME)
+    if not chat.messages: 
+        system_message = SystemMessage(content="You're a useful and friendly assitant. Answer clearly and precisely.")
+        ai_message = AIMessage(content="Hello, how can I help you today?", additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
+        chat.add_messages([system_message, ai_message])
     chat_history = chat.messages
     result = []
     for message in chat_history:
+        if message.type == "system": continue
         result.append({
             "id":message.additional_kwargs.get("id"),
             "content": message.content,
