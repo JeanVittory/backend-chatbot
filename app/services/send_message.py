01495da11854fc197a9be2241388179a9dbd7442
JeanVittory <vittory.dev@gmail.com>
2025-04-16 17:19:37 -0500

    feat: :sparkles: Add user_id value to FirestoreChatMessageHistory to send message only to the collection of that user

--- app/services/send_message.py
+++ app/services/send_message.py
@@ -7,18 +7,13 @@ from app.config.dotenv import GROQ_API_KEY, PROJECT_ID, SESSION_ID, COLLECTION_N
 import datetime
 import uuid
 
-def send_message(message:str):
+def send_message(message:str, user_id:str):
     client = firestore.Client(project=PROJECT_ID)
-    chat = FirestoreChatMessageHistory(client=client, session_id=SESSION_ID, collection=COLLECTION_NAME)
+    chat = FirestoreChatMessageHistory(client=client, session_id=user_id, collection=COLLECTION_NAME)
     temperature_parsed = parser(TEMPERATURE_MODEL)
     max_tokens_parsed = parser(MAX_TOKENS)
     model = ChatGroq(api_key=GROQ_API_KEY, temperature=temperature_parsed, model=MODEL_NAME, max_tokens=max_tokens_parsed)
-    
-    if not chat.messages: 
-        system_message = SystemMessage(content="You're a useful and friendly assitant. Answer clearly and precisely.")
-        ai_message = AIMessage(content="Hello, how can I help you today?", additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
-        chat.add_messages([system_message, ai_message])
-    
+
     user_message = HumanMessage(content=message, additional_kwargs={"date_time": datetime.datetime.now(), "id": uuid.uuid4()})
     chat.add_user_message(user_message)
     ai_response = model.invoke(chat.messages)
